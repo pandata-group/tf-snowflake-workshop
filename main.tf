e22e22a9d30603b84caebd11e3325b294acbb7ad
Trever Ehrfurth <trever.ehrfurth@gmail.com>
2025-02-20 00:13:23 -0600

    missed

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "snowflake" {
     organization_name   = var.snowflake_organization
     user                = "SVC_TERRAFORM"
     role                = "SECURITYADMIN"
-    private_key         = file(var.snowflake_private_key)
+    private_key         = var.snowflake_private_key # use file(var.snowflake_private_key) if running local
     authenticator       = var.snowflake_authenticator
 
     preview_features_enabled = [
